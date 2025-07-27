6c00b6028ae67daa49b96471ffcc64e59ea183c9
Afolabi Legunsen <aphoe@outlook.com>
2025-07-27 17:37:59 +0100

    feat: Create a command to create filament resource view

---
+++ app/Console/Commands/MakeFilamentResourceViewCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeFilamentResourceViewCommand extends Command
+{
+    protected $signature = 'make:filament-resource-view {model}';
+
+    protected $description = 'Create a Filament resource view for a model';
+
+    public function handle(): void
+    {
+        $model = $this->argument('model');
+
+        $command = "php artisan make:filament-page View{$model} --resource={$model}Resource --type=ViewRecord";
+
+        $response = shell_exec($command);
+
+        $this->info($response);
+    }
+}
