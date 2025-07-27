4a21a70b119307c01548e138d5e2ac0fa23271ca
Afolabi Legunsen <aphoe@outlook.com>
2025-07-27 17:38:36 +0100

    feat: Add company model

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\NavigationGroup;
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Models\Company;
+use Closure;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+
+    protected static ?string $slug = 'companies';
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
+
+    public static function getNavigationGroup(): ?string
+    {
+        return NavigationGroup::MISC->label();
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->columnSpan("full")
+                    ->required(),
+
+                TextInput::make('url')
+                    ->url(),
+
+                TextInput::make('email')
+                    ->email(),
+
+                MarkdownEditor::make('description')
+                    ->columnSpan("full"),
+
+                Placeholder::make('created_at')
+                    ->label('Created Date')
+                    ->content(fn(?Company $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+
+                Placeholder::make('updated_at')
+                    ->label('Last Modified Date')
+                    ->content(fn(?Company $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            /*->recordUrl(
+                fn (Model $record): string => route('filament.user.resources.companies.view', ['record' => $record]),
+            )*/
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('url'),
+
+                TextColumn::make('description'),
+
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'create' => Pages\CreateCompany::route('/create'),
+            'view' => Pages\ViewCompany::route('/{record}'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['name', 'email'];
+    }
+}
---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class CompanyFactory extends Factory
+{
+    protected $model = Company::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'url' => $this->faker->url(),
+            'description' => $this->faker->text(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
