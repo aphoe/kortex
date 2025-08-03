3f5dc8c17c06e233a394802e3e9d171548e6f5be
Afolabi Legunsen <aphoe@outlook.com>
2025-08-03 13:30:07 +0100

    feat: Change log viewer package

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('user');
 });
+
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index'])
+    ->middleware('auth');
